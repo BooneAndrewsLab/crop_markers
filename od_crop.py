1ab8533101e824c69b6cbe00fa2c5e3ebc0e2054
usajusaj <usajusaj@gmail.com>
2019-11-20 15:15:21 -0500

    Support different input file formats (channels, fields)

--- od_crop.py
+++ od_crop.py
@@ -17,7 +17,10 @@ def main():
     parser.add_argument("coordinates", help="File with cell coordinates", type=argparse.FileType('r'))
     parser.add_argument("base_folder", help="Root folder for images", type=lambda x: pathlib.Path(x))
     parser.add_argument("output_folder", help="Recreate input structure in this folder", type=lambda x: pathlib.Path(x))
+    parser.add_argument("-f", "--field", choices=['single', 'multi'], default='single')
+    parser.add_argument("-c", "--channels", default=2)
     parser.add_argument("-s", "--crop-size", help="Size of cropped cells", default=64)
+
     args = parser.parse_args()
 
     log.info("Reading coordinates")
@@ -26,7 +29,9 @@ def main():
 
     # Filter border cells
     log.info("Finding border cells, %d pixels from border", half_crop)
-    dim_ch, dim_y, dim_x = imread(str(args.base_folder / locs.loc[0, 'path']), plugin='tifffile').shape
+    img = imread(str(args.base_folder / locs.loc[0, 'path']), plugin='tifffile')
+    dim_y, dim_x = img.shape[-2:]
+
     good_y = locs.loc[:, 'center_y'].apply(lambda y: (y >= half_crop) & (y <= dim_y - half_crop))
     good_x = locs.loc[:, 'center_x'].apply(lambda x: (x >= half_crop) & (x <= dim_x - half_crop))
     mask = good_x & good_y
@@ -44,6 +49,10 @@ def main():
         log.info("Processing %d/%d", count, all_images)
         img = imread(str(args.base_folder / image_path), plugin='tifffile')
 
+        if args.channels == 1:
+            # Mojca is "special"
+            img = img.reshape((1,) + img.shape)
+
         crops_path = args.output_folder / image_path
         crops_path.parent.mkdir(parents=True, exist_ok=True)
 
@@ -51,15 +60,20 @@ def main():
             crops_path,
             dtype=img.dtype,
             mode='w+',
-            shape=(cell_data.shape[0], 2, args.crop_size, args.crop_size)
+            shape=(cell_data.shape[0], args.channels, args.crop_size, args.crop_size)
         )
 
         internal_cell_id = 0
         for locs_idx, row in cell_data.iterrows():
+            field = 0
+            if args.field == 'multi':
+                field = row.field
+
             crops_map[internal_cell_id] = img[
-                                          :,
+                                          (field * 2):(field * 2) + 2,
                                           row.center_y - half_crop:row.center_y + half_crop,
                                           row.center_x - half_crop:row.center_x + half_crop]
+
             cell_id_map.append((locs_idx, internal_cell_id))
             internal_cell_id += 1
 
