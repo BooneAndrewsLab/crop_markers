be404e9025cec300ef0d277704479453163c866c
usajusaj <usajusaj@gmail.com>
2019-11-21 11:16:01 -0500

    Should work

--- od_crop.py
+++ od_crop.py
@@ -28,27 +28,28 @@ def main():
     half_crop = args.crop_size // 2
     log.info("Half crop is %d", half_crop)
 
-    # Filter border cells
-    log.info("Finding border cells, %d pixels from border", half_crop)
-    img = imread(str(args.base_folder / locs.loc[0, 'path']), plugin='tifffile')
-    dim_y, dim_x = img.shape[-2:]
-
-    good_y = locs.loc[:, 'center_y'].apply(lambda y: (y >= half_crop) & (y < dim_y - half_crop))
-    good_x = locs.loc[:, 'center_x'].apply(lambda x: (x >= half_crop) & (x < dim_x - half_crop))
-    mask = good_x & good_y
-
-    log.info("Removing %d border cells", (~mask).sum())
-    locs = locs.loc[mask]
+    locs.loc[:, 'internal_cell_id'] = -1
 
     args.output_folder.mkdir(exist_ok=True, parents=True)
 
-    cell_id_map = []
+    output_path = args.output_folder / pathlib.Path(args.coordinates.name).name
+    if output_path.exists():
+        os.remove(output_path)
 
     count = 1
     all_images = locs['path'].unique().shape[0]
     for image_path, cell_data in locs.groupby('path'):
         img = imread(str(args.base_folder / image_path), plugin='tifffile')
-        log.info("Processing %d/%d: (%r) %s", count, all_images, img.shape, image_path)
+
+        dim_y, dim_x = img.shape[-2:]
+        good_y = cell_data.loc[:, 'center_y'].apply(lambda y: (y >= half_crop) & (y < dim_y - half_crop))
+        good_x = cell_data.loc[:, 'center_x'].apply(lambda x: (x >= half_crop) & (x < dim_x - half_crop))
+        mask = good_x & good_y
+
+        all_cells = cell_data.shape[0]
+        cell_data = cell_data.loc[mask]
+
+        log.info("Processing %d/%d: (%d/%d cells) %s", count, all_images, cell_data.shape[0], all_cells, image_path)
 
         if args.channels == 1:
             # Mojca is "special"
@@ -75,20 +76,22 @@ def main():
                                           row.center_y - half_crop:row.center_y + half_crop,
                                           row.center_x - half_crop:row.center_x + half_crop]
 
-            cell_id_map.append((locs_idx, internal_cell_id))
+            cell_data.at[locs_idx, 'internal_cell_id'] = internal_cell_id
             internal_cell_id += 1
+
+        cell_data.to_csv(output_path, index=False, mode='a', header=(count == 1))
 
         del crops_map
         count += 1
 
-    log.info("Attaching internal cell indexing column")
-    df_map = p.DataFrame(cell_id_map, columns=['idx', 'internal_cell_id'], dtype=int)
-    df_map = df_map.set_index('idx')
-    locs = locs.join(df_map, how='inner')
-
-    output_path = args.output_folder / pathlib.Path(args.coordinates.name).name
-    log.info("Saving modified coordinates to %s", output_path)
-    locs.to_csv(output_path, index=False)
+    # log.info("Attaching internal cell indexing column")
+    # df_map = p.DataFrame(cell_id_map, columns=['idx', 'internal_cell_id'], dtype=int)
+    # df_map = df_map.set_index('idx')
+    # locs = locs.join(df_map, how='inner')
+    #
+    # output_path = args.output_folder / pathlib.Path(args.coordinates.name).name
+    # log.info("Saving modified coordinates to %s", output_path)
+    # locs.to_csv(output_path, index=False)
 
     log.info("DONE")
 
